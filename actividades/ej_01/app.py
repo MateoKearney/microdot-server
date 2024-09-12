bb9bdc501303448a16b412ab67c9379527339bdd
Mateo Kearney <mateokearney80@gmail.com>
2024-09-12 14:22:09 -0300

    feat: adjunto ejercicio 1

--- actividades/ej_01/app.py
+++ actividades/ej_01/app.py
@@ -1,1 +1,14 @@
-# Aplicacion del servidor
+from microdot import send_file
+from microdot import Microdot
+
+app = Microdot()
+
+@app.route('/')
+async def index(request):
+    return send_file('index.html')
+
+@app.route("/<dir>/<file>")
+async def static(request, dir, file):
+    return send_file("/"+ dir + "/" + file)
+
+app.run(port = 80)
